7e28da20d6b66a1f4b2a6da2aaa1412ee5b62829
pablo <pienkowskip@gmail.com>
2014-05-24 11:18:21 +0200

    Cache session store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Pienkowski::Application.config.session_store :cookie_store, key: '_pienkowski_session'
+Pienkowski::Application.config.session_store :cache_store
